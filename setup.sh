9254f841eb688c0b8993546d4e9e603beb3efec2
Geoff Massanek <gmassanek@gmail.com>
2015-04-18 17:45:45 -0500

    Got my local working

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 echo 'Symlinking .ackrc'
-ln -s .dot_files/.ackrc .ackrc
+ln -s .dot_files/.ackrc ~/.ackrc
 
 echo 'Symlinking .gemrc'
-ln -s .dot_files/.gemrc .gemrc
+ln -s .dot_files/.gemrc ~/.gemrc
 
 echo 'Symlinking .gitconfig'
-ln -s .dot_files/.gitconfig .gitconfig
+ln -s .dot_files/.gitconfig ~/.gitconfig
 
 echo 'Symlinking .gitignore'
-ln -s .dot_files/.gitignore .gitignore
+ln -s .dot_files/.gitignore ~/.gitignore
 
 echo 'Symlinking .rspec'
-ln -s .dot_files/.rspec .rspec
+ln -s .dot_files/.rspec ~/.rspec
 
 echo 'Symlinking .rvmrc'
-ln -s .dot_files/.rvmrc .rvmrc
+ln -s .dot_files/.rvmrc ~/.rvmrc
 
 echo 'Symlinking .vim'
 rm -rf .vim
-ln -s .dot_files/.vim .vim
+ln -s .dot_files/.vim ~/.vim
 
 echo 'Symlinking .vimrc'
-ln -s .dot_files/.vimrc .vimrc
+ln -s .dot_files/.vimrc ~/.vimrc
