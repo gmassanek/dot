22081a7e45e533c817fe1be3084a2803badf399e
Geoff Massanek <geoff@reverb.com>
2019-08-26 11:45:33 -0500

    Latest

--- setup.sh
+++ setup.sh
@@ -24,9 +24,6 @@ ln -s .dot_files/.githelpers ~/.githelpers
 echo 'Symlinking .rspec'
 ln -s .dot_files/.rspec ~/.rspec
 
-echo 'Symlinking .rvmrc'
-ln -s .dot_files/.rvmrc ~/.rvmrc
-
 echo 'Symlinking .vim'
 rm -rf .vim
 ln -s .dot_files/.vim ~/.vim
@@ -38,6 +35,47 @@ echo 'Install brew'
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo 'Install brew stuff'
-brew install vim
-brew install go
+brew tap "caskroom/cask"
+brew tap "reverbdotcom/rv"
+brew tap "versent/homebrew-taps"
+
+brew install node@10
+
+#brew install "nodenv"
+brew install "awscli"
+brew install "bash"
+brew install "bash-completion"
+brew install "convox"
+brew install "curl"
+brew install "findutils"
+brew install "git"
+brew install "gnu-sed"
+brew install "go"
+brew install "icu4c"
+brew install "jq"
+brew install "libxml2"
+brew install "libxslt"
+brew install "macvim"
+brew install "nginx"
+brew install "nvim"
+brew install "openssl"
+brew install "overmind"
+brew install "pipenv"
+brew install "postgres"
+brew install "protobuf"
+brew install "protoc-gen-go"
+brew install "pyenv"
+brew install "rbenv"
+brew install "readline"
+brew install "ruby-build"
+brew install "rv"
+brew install "the_silver_searcher"
+brew install "vcprompt"
+brew install "vim"
+brew install "wget"
+brew install "yarn", args: ["ignore-dependencies"]
+brew install neovim
+brew install saml2aws
+cask install "docker"
+cask install "visual-studio-code"
 
